433f41e04895a235731e42c18ad0970d5967ae99
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-16 12:21:59 +0530

    Video state watched

--- App.js
+++ App.js
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    };
+  }
+
+  update(e) {
+    this.setState({txt: e.target.value});
+  }
+
   render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>;
+    return (
+      <div>
+        <input type="text"
+          onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
-ReactDOM.render(
-  <App cat={5} />,
-  document.querySelector("#app")
-);
+export default App;
