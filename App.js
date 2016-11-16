96e957c84de2ab80adc2fb178b9d06c56e19cc66
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-16 12:28:05 +0530

    Watched child components

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
+    this.update = this.update.bind(this);
     this.state = {
-      txt: 'this is the state txt',
-      cat: 0
+      txt: '',
     };
   }
 
@@ -17,12 +17,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text"
-          onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     );
   }
+}
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text"
+        onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
 }
 
 export default App;
