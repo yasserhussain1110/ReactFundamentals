81d55dfa3ed77d7031fe303226089f7f0f9168d0
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-16 12:44:52 +0530

    Watched refs

--- App.js
+++ App.js
@@ -6,33 +6,48 @@ class App extends React.Component {
     super();
     this.update = this.update.bind(this);
     this.state = {
-      txt: '',
+      red: 0,
+      green: 0,
+      blue: 0
     };
   }
 
   update(e) {
-    this.setState({txt: e.target.value});
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    });
   }
 
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br />
       </div>
     );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text"
-        onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  );
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+      <input ref="inp" type="range"
+        min="0"
+        max="255"
+        onChange={this.props.update} />
+      </div>
+    );
+  }
 }
 
 export default App;
