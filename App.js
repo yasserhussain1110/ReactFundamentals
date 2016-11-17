6129abd7c9e26aa14c1de0c372d86c12ac8a8339
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-17 15:05:15 +0530

    Control React Component updates when new props

--- App.js
+++ App.js
@@ -4,51 +4,34 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {val: 0};
     this.update = this.update.bind(this);
+    this.state = {increasing: false};
   }
   update() {
-    this.setState({val: this.state.val + 1 });
-  }
-  componentWillMount() {
-    this.setState({m: 2});
-  }
-  render() {
-    console.log('rendering!');
-    return <button onClick={this.update}>{this.state.val * this.state.m}</button>;
-  }
-  componentDidMount(){
-    console.log(ReactDOM.findDOMNode(this));
-    this.inc = setInterval(this.update, 500);
-  }
-  componentWillUnmount(){
-    console.log('bye!');
-    clearInterval(this.inc);
-  }
-}
-
-
-class Wrapper extends React.Component {
-  constructor() {
-    super();
-    this.mount = this.mount.bind(this);
-    this.unmount = this.unmount.bind(this);
-  }
-  mount() {
-    ReactDOM.render(<App />, document.getElementById('a'));
-  }
-  unmount() {
-    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.mount}>mount</button>
-        <button onClick={this.unmount}>unmount</button>
-        <div id="a"></div>
-      </div>
+    ReactDOM.render(
+      <App val={this.props.val+1} />,
+      document.getElementById('app')
     );
   }
+  componentWillReceiveProps(nextProps) {
+    this.setState({increasing: nextProps.val > this.props.val});
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.val % 5 === 0;
+  }
+  render(){
+    console.log(this.state.increasing);
+    return (
+      <button onClick={this.update}>
+        {this.props.val}
+      </button>
+    );
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log('prevProps', prevProps);
+  }
 }
 
-export default Wrapper;
+App.defaultProps = {val: 0};
+
+export default App;
