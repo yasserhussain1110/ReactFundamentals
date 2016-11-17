5a72622c9095772ac6e8c270b7ccec68c9284766
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-17 11:26:58 +0530

    Done understand components lifecycle methods

--- App.js
+++ App.js
@@ -2,18 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {val: 0};
+    this.update = this.update.bind(this);
+  }
+  update() {
+    this.setState({val: this.state.val + 1 });
+  }
+  componentWillMount() {
+    console.log('mounting');
+  }
   render() {
-    return <Button>I <Heart /> React</Button>;
+    console.log('rendering!');
+    return <button onClick={this.update}>{this.state.val}</button>;
+  }
+  componentDidMount(){
+    console.log('mounted');
+  }
+  componentWillUnmount(){
+    console.log('bye!');
   }
 }
 
-class Button extends React.Component {
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
   render() {
-    return <button>{this.props.children}</button>
+    return (
+      <div>
+        <button onClick={this.mount}>mount</button>
+        <button onClick={this.unmount}>unmount</button>
+        <div id="a"></div>
+      </div>
+    );
   }
 }
 
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
-
-export default App;
+export default Wrapper;
