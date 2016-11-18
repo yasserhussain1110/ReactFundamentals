12c8b475e88a482a6f697ca952681866503f1621
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-18 18:04:39 +0530

    Create components from arrays video watched

--- App.js
+++ App.js
@@ -4,68 +4,28 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      red: 0
-    };
-    this.update = this.update.bind(this);
+    this.state = {data: [
+      {id: 1, name: "Simon Bailey"},
+      {id: 2, name: "Thomas Burleson"},
+      {id: 3, name: "Will Button"},
+      {id: 4, name: "Ben Clinkinbeard"}
+    ]}
   }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
-    });
-  }
-  render() {
-    return <div>
-      <NumInput
-        ref="red"
-        min={0}
-        max={255}
-        step={1}
-        val={+this.state.red}
-        type="number"
-        label="Red"
-        update={this.update} />
-    </div>
+  render(){
+    let rows = this.state.data.map(person => {
+      return <PersonRow key={person.id} data={person} />;
+    })
+    return <table>
+      <tbody>{rows}</tbody>
+    </table>;
   }
 }
 
-class NumInput extends React.Component {
-  render() {
-    let label = this.props !== '' ?
-      <label>{this.props.label} - {this.props.val}</label> : '';
-    return (
-      <div>
-        <input ref="inp"
-          type={this.props.type}
-          min={this.props.min}
-          max={this.props.max}
-          step={this.props.step}
-          defaultValue={this.props.val}
-          onChange={this.props.update} />
-          {label}
-      </div>
-    );
-  }
-}
-
-
-NumInput.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  step: React.PropTypes.number,
-  val: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['number', 'range'])
-}
-
-NumInput.defaultProps = {
-  min: 0,
-  max: 0,
-  step: 1,
-  val: 0,
-  label: '',
-  type: 'range'
+const PersonRow = (props) => {
+  return <tr>
+    <td>{props.data.id}</td>
+    <td>{props.data.name}</td>
+  </tr>;
 }
 
 export default App;
